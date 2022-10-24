4ff8b49a07b984ea07be88028d4eb168b725a946
Valéria <valeriamenezes022@gmail.com>
2022-10-24 20:29:49 -0300

    alterações no requisito 5

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
-// import App from '../App';
 import Provider from '../context/Provider';
 import Login from '../components/Login';
 
 describe('Testando tela de Login', () => {
-  // const inputEmail = screen.getByTestId('email-input');
-  // const inputPassword = screen.getByTestId('password-input');
-  // const submitButton = screen.getByTestId('login-submit-btn');
   it('Verifica se os inputs estão na tela', () => {
     renderWithRouter(
       <Provider>
@@ -17,10 +13,18 @@ describe('Testando tela de Login', () => {
       </Provider>,
     );
 
+    const inputEmail = screen.getByTestId('email-input');
+    const inputPassword = screen.getByTestId('password-input');
+    const submitButton = screen.getByTestId('login-submit-btn');
+
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
     expect(submitButton).toBeInTheDocument();
     expect(submitButton).toHaveProperty('disabled', true);
+
+    userEvent.type(inputEmail, 'valeria@email');
+    userEvent.type(inputPassword, '1234567');
+    userEvent.click(submitButton);
   });
   it('Verifica se o botão está desabilitado', () => {
     renderWithRouter(
@@ -29,23 +33,12 @@ describe('Testando tela de Login', () => {
       </Provider>,
     );
 
-    userEvent.type(inputEmail, 'valeria-menezes');
-    userEvent.type(inputPassword, '123');
-    expect(submitButton).toBeDisabled();
-  });
-  it('Testa se o botão está habiltado', () => {
-    renderWithRouter(
-      <Provider>
-        <Login />
-      </Provider>,
-    );
     const inputEmail = screen.getByTestId('email-input');
     const inputPassword = screen.getByTestId('password-input');
     const submitButton = screen.getByTestId('login-submit-btn');
 
-    userEvent.type(inputEmail, 'valeria@email');
-    userEvent.type(inputPassword, '1234567');
-    userEvent.click(submitButton);
-    // expect(submitButton).not.toBeDisabled();
+    userEvent.type(inputEmail, 'valeria-menezes');
+    userEvent.type(inputPassword, '123');
+    expect(submitButton).toBeDisabled();
   });
 });
