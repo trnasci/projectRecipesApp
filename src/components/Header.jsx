42a5cb1b856eed4df9bd16ee294578078b6cd468
Nelson D'Vasconcelos <nelsonfilho1984@gmail.com>
2022-10-25 20:45:46 -0300

    requisito 11 - promise problem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
+import Context from '../context/Context';
 
 function Header() {
   const [searchInput, setSearchInput] = useState(false);
   const location = useLocation();
   const { pathname } = location;
+  const { title, setTitle, haveSearchIcon, setHaveSearchIcon } = useContext(Context);
 
-  let haveSearchIcon = false;
-  if (pathname === '/drinks' || pathname === '/meals') {
-    haveSearchIcon = true;
-  }
-  let title;
-  if (pathname === '/done-recipes' || pathname === '/favorite-recipes') {
-    const str = pathname.substring(1, pathname.length);
-    const str2 = str.replace('-', ' ');
-    const arr = str2.split(' ');
-    const fristChar = arr[0].charAt(0).toUpperCase();
-    const secondChar = arr[1].charAt(0).toUpperCase();
-    const str3 = arr[0].slice(1);
-    const str4 = arr[1].slice(1);
-    title = `${fristChar}${str3} ${secondChar}${str4}`;
-  } else {
-    const str = pathname.substring(1, pathname.length);
-    const fristChar = str.charAt(0).toUpperCase();
-    const str2 = str.slice(1);
-    title = fristChar + str2;
-  }
+  useEffect(() => {
+    if (pathname === '/drinks' || pathname === '/meals') {
+      setHaveSearchIcon(true);
+    }
+
+    if (pathname === '/done-recipes' || pathname === '/favorite-recipes') {
+      const str = pathname.substring(1, pathname.length);
+      const str2 = str.replace('-', ' ');
+      const arr = str2.split(' ');
+      const firstChar = arr[0].charAt(0).toUpperCase();
+      const secondChar = arr[1].charAt(0).toUpperCase();
+      const str3 = arr[0].slice(1);
+      const str4 = arr[1].slice(1);
+      setTitle(`${firstChar}${str3} ${secondChar}${str4}`);
+    } else {
+      const str = pathname.substring(1, pathname.length);
+      const firstChar = str.charAt(0).toUpperCase();
+      const str2 = str.slice(1);
+      setTitle(firstChar + str2);
+    }
+  }, [pathname, setHaveSearchIcon, setTitle]);
 
   /*  const handleSearch({ target }) => {
 
