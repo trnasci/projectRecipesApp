75e34d0d605f36899acf475123ece6bd8c1b70d8
Valéria <valeriamenezes022@gmail.com>
2022-10-26 00:27:18 -0300

    adiciona requisito 57

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,6 +6,32 @@ function Profile() {
   return (
     <div>
       <Header />
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input
+            data-testid="profile-email"
+          />
+        </label>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+        >
+          Logout
+        </button>
+      </form>
       <Footer />
     </div>
   );
